6a45eda7be1f5e20c29c8571e106b43599f97a89
markymouseee <sanamamarknoriel9@gmail.com>
2024-09-26 16:37:20 +0800

    Problem 7 done

--- Problem7.java
+++ Problem7.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Problem7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the day portion of day: ");
+        int day = scanner.nextInt();
+
+        if(day > 31 || day < 1){
+            System.out.println("Invalid Day");
+        }else{
+            System.out.println("Correct");
+        }
+
+        scanner.close();
     }
 }
